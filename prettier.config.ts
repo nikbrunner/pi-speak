20b7ada0c1ba60859d04afb364effe770bffe8dd
nbr <nikolaus.brunner@protonmail.ch>
2026-04-10 20:52:24 +0200

    ci: dynamically load Node version from package.json

--- prettier.config.ts
+++ prettier.config.ts
@@ -13,10 +13,7 @@ const config: Config = {
   bracketSameLine: false,
   arrowParens: "avoid",
   endOfLine: "lf",
-
-  // NOTE: Tailwind plugin must be last in the array (https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins)
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
-
   importOrder: ["<THIRD_PARTY_MODULES>", "^[.]"],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderTypeScriptVersion: "5.0.0"
